5b2824e1cf44da0e4aee2c8d928bc1600dc26607
沙智彬 <shazhibin@bonc.com.cn>
2019-12-31 11:29:45 +0800

    调整配置文件切换本地和生产启动

--- demo-controller/src/main/java/com/demo/controller/UserController.java
+++ demo-controller/src/main/java/com/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.demo.beans.User;
 import com.demo.service.UserService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    @Reference(group = "demo", url = "localhost:20880")
+    @Reference(group = "demo", url = "${control-dubbo.service.url}")
     private UserService userService;
 
-    @RequestMapping("/getUser")
+    @GetMapping("/getUser")
     public User getUser(Integer id) {
         return userService.getUser(id);
     }
