aa8d5f67fb86c0b54ccd3f9e0ad886e7b9c2f2e6
沙智彬 <shazhibin@bonc.com.cn>
2020-01-02 14:32:39 +0800

    service层整合redis集群

--- demo-service-impl/src/main/java/com/demo/service/impl/UserServiceImpl.java
+++ demo-service-impl/src/main/java/com/demo/service/impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.demo.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.demo.beans.User;
 import com.demo.dao.UserMapper;
 import com.demo.service.UserService;
 import org.apache.dubbo.config.annotation.Service;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.concurrent.TimeUnit;
 
 @Service(group = "demo")
 public class UserServiceImpl implements UserService {
@@ -15,9 +20,22 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userDao;
 
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
     @Override
     public User getUser(Integer id) {
+        ValueOperations<String, Object> opsVal = redisTemplate.opsForValue();
+        Object o = opsVal.get(String.valueOf(id));
+        if (o != null) {
+            return JSON.parseObject((String) o, User.class);
+        }
         LOGGER.info("controller查询数据" + id);
-        return userDao.getUser(id);
+        User user = userDao.getUser(id);
+        if (user != null) {
+            LOGGER.info("查询到的数据" + JSON.toJSONString(user));
+            opsVal.set(String.valueOf(id), JSON.toJSONString(user), 600, TimeUnit.SECONDS);
+        }
+        return user;
     }
 }
